33d4ab343dbd03ade4280b3f0f3ca0c38f05bb6c
liam.shaw <liam.shaw@MU-C1-06.mullen.pvt.k12.co.us>
2018-08-27 08:45:14 -0600

    Starting off my for loop challenges

---
+++ forLoops/src/forLoops/forLoops.java
@@ -0,0 +1,33 @@
+package forLoops;
+
+public class forLoops
+	{
+
+		public static void main(String[] args)
+			{
+				// TODO Auto-generated method stub
+				showGeniusOne();
+				showGeniusTwo();
+				showGeniusThree();
+				showGeniusFour();
+			}
+		public static void showGeniusOne()
+			{
+				for (int i = 1; i <=5; i++)
+					{
+						System.out.println(i);
+					}
+			}
+		public static void showGeniusTwo()
+			{
+			
+			}
+		public static void showGeniusThree()
+			{
+				
+			}
+		public static void showGeniusFour()
+			{
+			
+			}
+	}
